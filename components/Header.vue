d72fb3c21b163bc1d7b659a3da076d41bf4ed4c8
Florent Catiau-Tristant <florent.ct@orange.fr>
2021-06-25 20:41:49 +0200

    feat: add social network links

--- components/Header.vue
+++ components/Header.vue
@@ -1,12 +1,28 @@
 <template>
-  <div class="flex flex-wrap flex-col md:flex-row justify-start items-center">
-    <img src="~/assets/img/moi.jpg" alt="My smiling face" height="200px" width="200px" class="rounded-full object-cover border-4 border-pink-900">
+  <div class="flex flex-wrap flex-col md:flex-row justify-start items-start">
+    <div>
+      <img src="~/assets/img/moi.jpg" alt="My smiling face" height="200px" width="200px" class="rounded-full object-cover border-4 border-pink-900">
+      <div id="social-networks" class="flex justify-between my-2 px-8">
+        <a href="https://twitter.com/Kapcash">
+          <TwitterLogo height="24" width="24" fill="black" />
+        </a>
+        <a href="https://github.com/Kapcash">
+          <GithubLogo height="24" width="24" fill="black" />
+        </a>
+        <a href="https://dev.to/Kapcash">
+          <DevtoLogo height="24" width="24" fill="black" />
+        </a>
+        <a href="https://www.linkedin.com/in/florent-catiau-tristant/">
+          <LinkedInLogo height="24" width="24" fill="black" />
+        </a>
+      </div>
+    </div>
     <div class="flex flex-col flex-wrap px-4 justify-center md:justify-start flex-1">
-      <h1 class="my-3 font-bold small-caps text-4xl">
+      <h1 class="my-3 font-bold text-4xl">
         Hey! I'm Florent!
       </h1>
-      <p class="my-3 tracking-widest uppercase text-3xl">
-        I am a French web developer based in Barcelona!
+      <p class="my-3 tracking-widest text-3xl">
+        I am a French web developer based in Barcelona contributing to open source libraries.
       </p>
       <ul class="my-3">
         <li v-for="tag of tags" :key="tag" class="tag">
@@ -19,11 +35,19 @@
 
 <script lang="ts">
 import { Vue, Component } from 'nuxt-property-decorator'
-import GithubCard from '~/components/GithubCard.vue'
+import GithubCard from '@/components/GithubCard.vue'
+import TwitterLogo from '@/assets/svg/social-networks/twitter.svg'
+import LinkedInLogo from '@/assets/svg/social-networks/linkedin.svg'
+import GithubLogo from '@/assets/svg/social-networks/Github.svg'
+import DevtoLogo from '@/assets/svg/social-networks/devto.svg'
 
 @Component({
   components: {
-    GithubCard
+    GithubCard,
+    TwitterLogo,
+    LinkedInLogo,
+    GithubLogo,
+    DevtoLogo
   }
 })
 export default class HomePage extends Vue {
@@ -41,11 +65,13 @@ img {
   flex: 1;
 }
 
-.small-caps {
-  font-variant-caps: small-caps;
-}
-
 .tag {
   @apply inline text-pink-800 text-xl font-semibold mr-4;
+}
+
+svg:hover {
+  fill: rgb(131, 24, 67);
+  transform: rotate(-8deg);
+  transition: transform 200ms ease-out;
 }
 </style>
