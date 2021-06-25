50f23912bed8b5db7be8d846ebc666d2b0786998
Florent Catiau-Tristant <florent.ct@orange.fr>
2021-06-25 20:52:33 +0200

    feat: move social networks links to component

--- components/Header.vue
+++ components/Header.vue
@@ -2,20 +2,7 @@
   <div class="flex flex-wrap flex-col md:flex-row justify-start items-start">
     <div>
       <img src="~/assets/img/moi.jpg" alt="My smiling face" height="200px" width="200px" class="rounded-full object-cover border-4 border-pink-900">
-      <div id="social-networks" class="flex justify-between my-2 px-8">
-        <a href="https://twitter.com/Kapcash">
-          <TwitterLogo height="24" width="24" fill="black" />
-        </a>
-        <a href="https://github.com/Kapcash">
-          <GithubLogo height="24" width="24" fill="black" />
-        </a>
-        <a href="https://dev.to/Kapcash">
-          <DevtoLogo height="24" width="24" fill="black" />
-        </a>
-        <a href="https://www.linkedin.com/in/florent-catiau-tristant/">
-          <LinkedInLogo height="24" width="24" fill="black" />
-        </a>
-      </div>
+      <SocialNetworks />
     </div>
     <div class="flex flex-col flex-wrap px-4 justify-center md:justify-start flex-1">
       <h1 class="my-3 font-bold text-4xl">
@@ -35,22 +22,9 @@
 
 <script lang="ts">
 import { Vue, Component } from 'nuxt-property-decorator'
-import GithubCard from '@/components/GithubCard.vue'
-import TwitterLogo from '@/assets/svg/social-networks/twitter.svg'
-import LinkedInLogo from '@/assets/svg/social-networks/linkedin.svg'
-import GithubLogo from '@/assets/svg/social-networks/Github.svg'
-import DevtoLogo from '@/assets/svg/social-networks/devto.svg'
 
-@Component({
-  components: {
-    GithubCard,
-    TwitterLogo,
-    LinkedInLogo,
-    GithubLogo,
-    DevtoLogo
-  }
-})
-export default class HomePage extends Vue {
+@Component
+export default class Header extends Vue {
   readonly tags = ['vue', 'nuxt', 'nestjs']
 }
 </script>
@@ -67,11 +41,5 @@ img {
 
 .tag {
   @apply inline text-pink-800 text-xl font-semibold mr-4;
-}
-
-svg:hover {
-  fill: rgb(131, 24, 67);
-  transform: rotate(-8deg);
-  transition: transform 200ms ease-out;
 }
 </style>
---
+++ components/SocialNetworks.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="social-networks flex my-2 justify-center">
+    <a href="https://twitter.com/Kapcash">
+      <TwitterLogo height="24" width="24" fill="black" />
+    </a>
+    <a href="https://github.com/Kapcash">
+      <GithubLogo height="24" width="24" fill="black" />
+    </a>
+    <a href="https://dev.to/Kapcash">
+      <DevtoLogo height="24" width="24" fill="black" />
+    </a>
+    <a href="https://www.linkedin.com/in/florent-catiau-tristant/">
+      <LinkedInLogo height="24" width="24" fill="black" />
+    </a>
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from 'nuxt-property-decorator'
+import GithubCard from '@/components/GithubCard.vue'
+import TwitterLogo from '@/assets/svg/social-networks/twitter.svg'
+import LinkedInLogo from '@/assets/svg/social-networks/linkedin.svg'
+import GithubLogo from '@/assets/svg/social-networks/Github.svg'
+import DevtoLogo from '@/assets/svg/social-networks/devto.svg'
+
+@Component({
+  components: {
+    GithubCard,
+    TwitterLogo,
+    LinkedInLogo,
+    GithubLogo,
+    DevtoLogo
+  }
+})
+export default class SocialNetworks extends Vue {}
+</script>
+
+<style lang="postcss" scoped>
+.social-networks > * {
+  margin-left: 8px;
+  margin-right: 8px;
+}
+
+svg:hover {
+  fill: rgb(131, 24, 67);
+  transform: rotate(-8deg);
+  transition: transform 200ms ease-out;
+}
+</style>
