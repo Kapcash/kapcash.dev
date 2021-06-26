f3c8840a63ca8f2ecdb96bbc9c884ae8d1b5e903
Florent Catiau-Tristant <florent.ct@orange.fr>
2021-06-26 11:01:52 +0200

    fix: footer grid layout on mobile

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,11 +1,11 @@
 <template>
-  <footer class="mt-10 bg-gray-100 py-6 grid grid-flow-col grid-cols-3 auto-cols-max justify-items-center items-center">
-    <div class="col-start-2 text-center">
+  <footer class="-mx-3 mt-10 bg-gray-100 py-6 grid grid-flow-col grid-rows-2 md:grid-rows-none grid-cols-none md:grid-cols-3 gap-y-4 justify-items-center items-center">
+    <div class="md:col-start-2 text-center">
       <SocialNetworks />
       <p>©{{ currentYear }} Developed by Florent Catiau-Tristant</p>
     </div>
 
-    <BuyMeACoffee class="col-start-3" />
+    <BuyMeACoffee class="md:col-start-3" />
   </footer>
 </template>
 
