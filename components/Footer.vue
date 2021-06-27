0859ec34d5366172bc8b887ecbd7a04cad1e86fe
Florent Catiau-Tristant <florent.ct@orange.fr>
2021-06-27 18:18:10 +0200

    feat: footer no margin on desktop

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,5 +1,5 @@
 <template>
-  <footer class="-mx-3 mt-10 bg-gray-100 py-6 grid grid-flow-col grid-rows-2 md:grid-rows-none grid-cols-none md:grid-cols-3 gap-y-4 justify-items-center items-center">
+  <footer class="-mx-3 md:mx-0 mt-10 bg-gray-100 py-6 grid grid-flow-col grid-rows-2 md:grid-rows-none grid-cols-none md:grid-cols-3 gap-y-4 justify-items-center items-center">
     <div class="md:col-start-2 text-center">
       <SocialNetworks />
       <p>©{{ currentYear }} Developed by Florent Catiau-Tristant</p>
