b360b3c6022eba7e6e24b17284417bb983d677a5
Florent Catiau-Tristant <florent.ct@orange.fr>
2023-04-11 00:09:32 +0200

    feat: Frame zoom on click only + author link

--- v2/components/Frame.vue
+++ v2/components/Frame.vue
@@ -1,6 +1,9 @@
 <template>
-  <div class="frame" :class="{ zoom }">
-    <img :src="imgSrc" class="object-contain max-h-full" @mouseover="zoom = true" @mouseleave="zoom = false">
+  <div class="relative frame-container" :class="{ zoom }">
+    <div class="frame" :class="zoom ? 'cursor-zoom-out' : 'cursor-zoom-in'" @click="zoom = !zoom">
+      <img :src="imgSrc" class="object-contain max-h-full">
+    </div>
+    <span class="absolute text-gray-500 text-[0.6em] -bottom-6 right-0">Frame css by <a class="text-blue-500 hover:text-blue-600" href="https://codepen.io/Kaiyuan/pen/DjBRbV" target="_blank">@Kaiyuan</a></span>
   </div>
 </template>
 
@@ -13,12 +16,20 @@ const zoom = ref(false)
 </script>
 
 <style scoped>
+.frame-container {
+  transition: transform 100ms ease-in-out;
+}
+.frame-container.zoom {
+  transform: scale(1.8);
+}
+
 /* Source: https://codepen.io/Kaiyuan/pen/DjBRbV */
 .frame {
   display: inline-block;
 	padding: 20px;
 	border-width: 20px;
 	border-style: solid;
+  height: 100%;
 	border-color: #2F2D2D #434040 #4F4C4C #434040;
 	background: #f5f5f5;
 	background-image: -webkit-gradient(linear, 0 0, 0 100%, from(#E5E4DF), to(#CDCDC6));
@@ -29,7 +40,6 @@ const zoom = ref(false)
 	box-shadow: inset 0 2px 5px rgba(0,0,0,.6),0 5px 2px rgba(0,0,0,.1), 0 10px 20px rgba(0,0,0,.8);
 	position: relative;
 	overflow: hidden;
-  transition: transform 100ms ease-in-out;
 }
 .frame::before {
 	content: "";
@@ -48,15 +58,19 @@ const zoom = ref(false)
 	background-image: -o-linear-gradient(rgba(255,255,255,.4), rgba(255,255,255,0));
 	background-image: linear-gradient(rgba(255,255,255,.4), rgba(255,255,255,0));
 }
+.frame::after {
+  /* content: '@Kaiyuan (https://codepen.io/Kaiyuan/pen/DjBRbV)'; */
+  font-size: 0.5em;
+  color: gray;
+  text-align: right;
+  position: absolute;
+  bottom: -2rem;
+  right: 0;
+}
 .frame img {
 	border-width: 2px;
 	border-style: solid;
 	border-color: #BBBAB4 #C7C7BF #E5E4DF #C7C7BF;
 	box-shadow:  0 -1px 1px rgba(0,0,0,.1), 0 1px 1px 1px rgba(255,255,255,.7);
-}
-
-.frame:hover {
-  cursor: zoom-in;
-  transform: scale(1.8);
 }
 </style>
